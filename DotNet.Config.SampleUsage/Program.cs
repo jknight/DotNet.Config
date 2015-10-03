4b53b0ec94a056f381474fcdd501a57caa372ea8
Jeffrey Knight <Jeffrey.Knight@gmail.com>
2015-10-02 20:07:47 -0400

    added more comments in sample code

--- DotNet.Config.SampleUsage/Program.cs
+++ DotNet.Config.SampleUsage/Program.cs
@@ -22,16 +22,23 @@ namespace DotNet.Config.SampleUsage
 
         static void Main(string[] args)
         {
-            //you can get settings explicitly  without "glueing" them on.
-            //This is useful in a static context.
+            // You can get settings explicitly  without "glueing" them on.
+            // This is useful in a static context.
 
-            string firstName = AppSettings.Retrieve("config.properties")["firstName"];
-            Console.WriteLine("Seting loaded from explicit config file name: " + firstName);
-
-            //Or assuming the default "config.properties":
+            // Using the default "config.properties":
             string firstName2 = AppSettings.Retrieve()["firstName"];
             Console.WriteLine("Setting loaded from default config file:" + firstName2);
 
+            // Of you can be explicit about the file name:
+            string firstName = AppSettings.Retrieve("config.properties")["firstName"];
+            Console.WriteLine("Seting loaded from explicit config file name: " + firstName);
+
+            // And of course you can grab it once for reuse if you prefer. But note it won't reload
+            //  across multiple calls anyway
+            var settings = AppSettings.Retrieve();
+            Console.WriteLine("Here is a setting from config.properties:" + settings["firstName"]);
+
+            // You can also glue properties directly onto the class (this is the simplest and easiest approach)
             new Program().Init();
 
             Console.ReadLine();
@@ -39,9 +46,13 @@ namespace DotNet.Config.SampleUsage
 
         public void Init()
         {
-            //or use a convention-over-configuration shorthand to glue them on:
-            AppSettings.GlueOnto(this); //<-- That's it !`
+            // Use a convention-over-configuration shorthand to glue them on. This assumes the file 'config.properties'
+            //   is in the same directory as this exe/dll
 
+            // One line of code to apply settings 
+            AppSettings.GlueOnto(this); //<-- That's it, magic !
+
+            // And nwo they're automatically available:
             Console.WriteLine("myInteger:" + this.myInteger);
             Console.WriteLine("firstName:" + this.firstName);
             Console.WriteLine("color:" + this.color.ToString());
